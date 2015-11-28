16b9341d1499252d5d3e6b3d5cc1d8831074d3b3
Jeam Alves <jeanmalves@gmail.com>
2015-11-28 21:55:06 -0200

    implementação do controller para consulta de riscos para a pessoa

---
+++ application/controllers/Pessoa.php
@@ -0,0 +1,43 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+include_once(APPPATH . "controllers/Base.php");
+
+class Product extends Base_Controller {
+	
+
+	 var $layout = 'layout_peera';
+
+    public function __construct() {
+    	parent::__construct();
+	}
+
+	public function index(){
+		
+		$this->data['data']['logos'] = $logos;
+		$this->load->view($this->layout, $this->data );
+	}
+
+	public function mostrarRisco(){
+
+		$post = (!empty($_POST)) ? true : false;
+
+		if($post)
+		{
+
+			$name = stripslashes($_POST['name']);
+			$email = trim($_POST['email']);
+			$subject = stripslashes($_POST['subject']);
+			$message = stripslashes($_POST['message']);
+
+			$error = '';
+
+			if(!$error)
+			{
+				
+				$this->data['data']['erro'] = TRUE;
+
+			}
+		}
+	}
+}
